04a6f91678b60b8519519f3ea1d1b6c3d36ebdf3
Joshua McMorris <mcmorrisj@spu.edu>
2018-10-31 14:44:02 -0700

    Adds base code for server to listen to a message

--- server/index.js
+++ server/index.js
@@ -5,3 +5,10 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.send('<h1>This is the BabbleBabble Chat Room server</h1>');
 });
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('message', function(message) {
+        console.log(message);
+    })
+  });
