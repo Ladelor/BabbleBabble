e3a249794d568bb75acb9b8b9cd6de87e773e34b
Ladelor <mcmorrisj@spu.edu>
2018-10-31 17:28:52 -0700

    Text entered is emited to the server in an event

--- chat-room/src/chat-room-components/ChatEntry.js
+++ chat-room/src/chat-room-components/ChatEntry.js
@@ -4,11 +4,18 @@ class ChatEntry extends Component {
   constructor(props){
     super(props);
     this.chatServer = props.chatServer;
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.chatServer.emit('message', document.getElementById("chat-input").value);
+  }
+
   render() {
     return (
-        <form id="chat-entry">
-          <input type="text" name="chat-input"></input>
+        <form id="chat-entry" onSubmit={this.handleSubmit}>
+          <input id="chat-input" type="text" name="chat-input"></input>
           <input type="submit" value="Submit"></input>
         </form>
     );
