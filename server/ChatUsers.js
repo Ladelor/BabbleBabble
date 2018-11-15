5da488e8efd3724e04ee705759969ce5caed7176
Joshua McMorris <mcmorrisj@spu.edu>
2018-11-14 19:29:15 -0800

    Adds base code for ChatUsers List on server

---
+++ server/ChatUsers.js
@@ -0,0 +1,32 @@
+//This should eventually be an interface for a DB
+class ChatUsers {
+    constructor() {
+        this.chatUsersDB = [];
+    }
+
+    addUser(user) {
+        let foundUser = false;
+        for (let i = 0; i < this.chatUsersDB.length; i++) {
+            if (this.chatUsersDB[i].username === user.username) {
+                foundUser = true;
+                break;
+            }
+        }
+
+        if (!foundUser) {
+            this.chatUsersDB.push(user);
+            return false
+        }
+        else {
+            return true;
+        }
+    }
+
+    removeUser(username) {
+        for (var i = this.chatUsersDB.length - 1; i >= 0; --i) {
+            if (chatUsersDB[i].username === username) {
+                chatUsersDB.splice(i, 1);
+            }
+        }
+    }
+}
