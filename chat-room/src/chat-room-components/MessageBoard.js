d442f41f77a93387a9f9d62132db38e73f1635e0
Ladelor <mcmorrisj@spu.edu>
2018-11-15 14:40:43 -0800

    Added functionality to only store a max number of messages on client. Currently set to 100

--- chat-room/src/chat-room-components/MessageBoard.js
+++ chat-room/src/chat-room-components/MessageBoard.js
@@ -5,22 +5,31 @@ class MessageBoard extends Component {
   constructor(props) {
     super(props);
     this.chatServer = props.chatServer;
+    this.maxMessages = 100;
     this.chatServer.on('message', this.addMessage.bind(this));
     this.chatServer.on('userEnteredServer', this.addMessage.bind(this));
     this.state = { messages: [] };
   }
 
   addMessage(message) {
+    if (this.state.messages.length >= this.maxMessages) {
+      this.removeMessage(0);
+    }
     this.setState(prevState => ({
       messages: [...prevState.messages, message]
     }));
-    console.log(this.state.messages);
+  }
+
+  removeMessage(index) {
+    this.setState((prevState) => ({
+      messages: prevState.messages.filter((_, i) => i !== index)
+    }));
   }
 
   getMessageComponents() {
     let messageComponents = [];
     this.state.messages.forEach(function(messageObject){
-      messageComponents.push(<Message messageObject={ messageObject } key={ messageObject.timeStamp }/>);
+      messageComponents.push(<Message messageObject={ messageObject } key={ messageObject.timeStamp } />);
     });
 
     return messageComponents;
